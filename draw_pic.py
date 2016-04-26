335f2155c3107fd2edf1747b72d4a7e4c52b1afd
chineshboy <chineshboy@hotmail.com>
2016-04-26 19:09:32 +0800

    refine picture

--- draw_pic.py
+++ draw_pic.py
@@ -6,6 +6,16 @@ from os import listdir, path, mkdir
 
 __author__ = 'Jian Xun'
 
+LABEL = {
+    'geocrowdgreedy': 'G-greedy',
+    'geocrowdnnp': 'G-nnp',
+    'geocrowdllep': 'G-llep',
+    'geotrucrowdgreedy': 'GT-greedy',
+    'geotrucrowdlo': 'GT-lo',
+    'rdbscsampling': 'RDB-sam',
+    'rdbscdivideandconquer': 'RDB-d&c'
+}
+
 
 def read_file(file_path):
     datas = []
@@ -30,19 +40,46 @@ def read_file(file_path):
     return datas
 
 
-def draw(data):
+def draw(data, suffix):
     line_width = 2
-    marker_size = 10
-    mew = 2
-    markers = ['+', 'x', '*', '^', 'o', 's', 'D']
+    markers = [
+        {
+            'marker': 's',
+            'mew': 1,
+            'markerSize': 14
+        }, {
+            'marker': '^',
+            'mew': 1,
+            'markerSize': 14
+        }, {
+            'marker': 'd',
+            'mew': 1,
+            'markerSize': 14
+        }, {
+            'marker': '*',
+            'mew': 1,
+            'markerSize': 16
+        }, {
+            'marker': '.',
+            'mew': 1,
+            'markerSize': 16
+        }, {
+            'marker': '+',
+            'mew': 2,
+            'markerSize': 14
+        }, {
+            'marker': 'x',
+            'mew': 2,
+            'markerSize': 14
+        }]
     legend_text_size = 18
 
     plots = []
     marker = 0
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize=(9, 9))
     for label in data['lines']:
-        p, = plt.plot(data['lines'][label], color='k', marker=markers[marker], mew=mew, label=label, markerSize=marker_size,
-                      lineWidth=line_width)
+        p, = plt.plot(data['lines'][label], color='k', label=LABEL[label],
+                      markerfacecolor='w', lineWidth=line_width, **markers[marker])
         plots.append(p)
         marker += 1
     plt.xticks(range(len(data['x_series'])), data['x_series'], size='medium')
@@ -53,7 +90,7 @@ def draw(data):
     # save picture into 'pics' directory
     if not path.isdir('./pics'):
         mkdir('./pics')
-    savefig('./pics/' + data['distribution'] + '_' + data['x_label'] + '_' + data['y_label'] + '.png')
+    savefig('./pics/' + data['distribution'] + '_' + data['x_label'] + '_' + data['y_label'] + '.' + suffix)
 
 if __name__ == '__main__':
     files = listdir('.')
@@ -62,4 +99,4 @@ if __name__ == '__main__':
             datas = read_file(file_name)
             for data in datas:
                 if data['y_label'] != 'worker_num' and data['y_label'] != 'task_num':
-                    draw(data)
+                    draw(data, 'eps')
