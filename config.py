bf35fd66f1f225ca061e7d9ea140c012312c77e3
chineshboy <chineshboy@hotmail.com>
2016-05-24 22:48:14 +0800

    adjust params

--- config.py
+++ config.py
@@ -18,14 +18,11 @@ output_order_worker_select = ['workerselectprogressive',
 # total_worker_num = 100000
 distribution = ['skew', 'gaus']  # , 'real']
 worker_per_instance = [150, 200, 250, 300, 400, 500]
-worker_per_instance_worker_select = [300, 350, 400, 450, 500]
 task_per_instance = [150, 200, 250, 300, 400, 500]
-task_per_instance_worker_select = [300, 350, 400, 450, 500]
 task_duration = [(1, 2), (2, 4), (4, 6), (6, 8)]
-task_duration_worker_select = [(2, 3), (3, 4), (4, 5), (5, 6)]
 task_requirement = [(1, 3), (3, 5), (5, 7), (7, 9)]
 task_confidence = [(0.65, 0.7), (0.75, 0.8), (0.8, 0.85), (0.85, 0.9)]
-worker_capacity = [(2, 3), (3, 4), (4, 5), (5, 6)]
+worker_capacity = [(1, 3), (3, 5), (5, 7), (7, 9)]
 worker_reliability = [(0.65, 0.7), (0.7, 0.75), (0.75, 0.8), (0.8, 0.85), (0.85, 0.9)]
 working_side_length = [(0.05, 0.1), (0.1, 0.15), (0.15, 0.2), (0.2, 0.25)]
 
@@ -40,14 +37,22 @@ default_setting = {
     'worker_reliability': (0.75, 0.8),
     'working_side_length': (0.05, 0.1)
 }
+
+
+worker_per_instance_worker_select = [200, 250, 300, 350, 400]
+task_per_instance_worker_select = [200, 250, 300, 350, 400]
+task_duration_worker_select = [(2, 3), (3, 4), (4, 5), (5, 6)]
+worker_capacity_worker_select = [(2, 3), (3, 4), (4, 5), (5, 6)]
+
+
 default_setting_worker_select = {
     'instance_num': 20,
-    'worker_per_instance': 300,
-    'task_per_instance': 300,
+    'worker_per_instance': 200,
+    'task_per_instance': 200,
     'task_duration': (2, 3),
     'task_requirement': (3, 5),
     'task_confidence': (0.75, 0.8),
-    'worker_capacity': (3, 4),
+    'worker_capacity': (2, 3),
     'worker_reliability': (0.75, 0.8),
     'working_side_length': (0.1, 0.15)
 }
@@ -65,6 +70,8 @@ def change_to(category):
         output_order = output_order_worker_select
         global task_duration
         task_duration = task_duration_worker_select
+        global worker_capacity
+        worker_capacity = worker_capacity_worker_select
 
 
 def get_default():
